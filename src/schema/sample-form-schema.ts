6e9cebf079174dac1cc230699d5debcd18bdad42
nkmryu <ryusei1597@gmail.com>
2024-07-31 23:13:56 +0900

    def shema with zod

--- src/schema/sample-form-schema.ts
+++ src/schema/sample-form-schema.ts
@@ -1,1 +1,19 @@
 import { z } from "zod";
+
+const castToValOrNull = <T extends Parameters<typeof z.preprocess>[1]>(
+  schema: T
+) => {
+  return z.preprocess((val) => {
+    if (typeof val === "string") {
+      const trimmedVal = val.trim();
+      return trimmedVal.length > 0 ? trimmedVal : null;
+    }
+  }, schema);
+};
+
+export const sampleFormSchema = z.object({
+  name: castToValOrNull(z.string()),
+  nullableName: castToValOrNull(z.string().nullable()),
+  selectedValue: castToValOrNull(z.string()),
+  nullableSelectedValue: castToValOrNull(z.string().nullable()),
+});
